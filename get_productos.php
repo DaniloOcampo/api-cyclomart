40bc9305c3c5f4f6841333d58251f43d96c80b30
Danilo <daniloplaza2234@gmail.com>
2025-05-27 18:06:05 -0500

    actualizacion categoria

--- get_productos.php
+++ get_productos.php
@@ -5,14 +5,14 @@ header("Access-Control-Allow-Methods: GET");
 
 include 'db.php';
 
-// En db.php definimos $mysqli
 $mysqli->set_charset("utf8");
 
-// Cambia esta URL por la tuya si cambia el dominio
+
 $base_url = "https://api-cyclomart-1.onrender.com/";
 
 $sql = "SELECT productos.id, productos.nombre, productos.precio, productos.imagen, 
-               productos.descripcion, productos.tipo, productos.stock, categorias.nombre AS categoria 
+               productos.descripcion, productos.tipo, productos.stock, 
+               categorias.nombre AS categoria, productos.categoria_id
         FROM productos
         JOIN categorias ON productos.categoria_id = categorias.id";
 
@@ -35,4 +35,5 @@ if ($result) {
 
 $mysqli->close();
 ?>
+
 
