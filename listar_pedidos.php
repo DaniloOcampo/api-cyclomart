7d279c2803d4f3adb50016c9e83916775a1d8d18
Danilo <daniloplaza2234@gmail.com>
2025-05-30 04:22:23 -0500

    pedidos

--- listar_pedidos.php
+++ listar_pedidos.php
@@ -5,13 +5,17 @@ require 'db.php';
 // Consulta para obtener los pedidos con total de productos
 $sql = "
     SELECT 
-        p.id, p.fecha, p.metodo_pago, p.total, p.estado,
-        COUNT(dp.id_producto) AS cantidad_total
+        p.id AS id,
+        p.fecha,
+        p.metodo_pago,
+        SUM(dp.cantidad) AS cantidadTotal,
+        p.total
     FROM pedidos p
-    JOIN detalle_pedido dp ON dp.id_pedido = p.id
+    LEFT JOIN detalle_pedido dp ON p.id = dp.id_pedido
     GROUP BY p.id
     ORDER BY p.fecha DESC
 ";
+
 
 
 $result = $mysqli->query($sql);
@@ -19,16 +23,16 @@ $result = $mysqli->query($sql);
 if ($result && $result->num_rows > 0) {
     $pedidos = [];
 
-    while ($row = $result->fetch_assoc()) {
-        $pedidos[] = [
-            "id" => (int)$row['id'],
-            "fecha" => $row['fecha'],
-            "metodoPago" => $row['metodo_pago'],
-            "total" => (float)$row['total'],
-            "estado" => $row['estado'],
-            "cantidadTotal" => (int)$row['cantidad_total']
-        ];
-    }
+while ($row = $result->fetch_assoc()) {
+    $pedidos[] = [
+        "id" => $row["id"],
+        "fecha" => $row["fecha"],
+        "metodoPago" => $row["metodo_pago"],
+        "cantidadTotal" => (int)$row["cantidadTotal"],
+        "total" => (float)$row["total"]
+    ];
+}
+
 
     echo json_encode($pedidos);
 } else {
